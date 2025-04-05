0ede163a3c4f011a0d0b354a9229e20baaa52ed0
BORAN YANG <boranann@umich.edu>
2025-04-05 10:39:01 -0400

    Implement tablet view

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+document.querySelector("body > header > nav > button").addEventListener("click", function(e) {
+    e.target.classList.toggle("show-pictures");
+    if (e.target.classList.contains("show-pictures")) {
+        e.target.innerHTML = "Show Pictures";
+        document.querySelectorAll(".pic").forEach(picture => {
+            picture.style.display = "none";
+        });
+    }
+    else {
+        e.target.innerHTML = "Hide Pictures";
+        document.querySelectorAll(".pic").forEach(picture => {
+            picture.style.display = "block";
+        });
+    }
+});
